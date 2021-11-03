1e7b05f095ab433ef30675dd232c8e4131d611a6
Sindre Seppola <sindre.seppola@oceandata.earth>
2021-11-03 15:18:28 +0100

    fix: (add fallback value to avoid x-forwarded-uri being undefined if header is missing)

--- src/utils.js
+++ src/utils.js
@@ -188,7 +188,7 @@ function get_parent_request_uri(req) {
     originalRequestURI = originalRequestURI.replace(/^(.+?)\/*?$/, "$1"); // remove all trailing slashes
     originalRequestURI += req.headers["x-forwarded-uri"];
   } else {
-    originalRequestURI += req.headers["x-forwarded-uri"];
+    originalRequestURI += req.headers["x-forwarded-uri"] || '';
   }
 
   //x-forwarded-port
